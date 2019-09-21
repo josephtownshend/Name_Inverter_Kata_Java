af756750ded7b0d98bb47230d647ccc0a37d7593
joseph townshend <joetownshend@mac.com>
2019-09-21 11:17:55 +0100

    refactor: created invert method

--- src/main/java/NameInverter.java
+++ src/main/java/NameInverter.java
@@ -10,6 +10,10 @@ public class NameInverter {
         if (nameParts.size() < 2)
             return nameParts.get(0);
 
+        return invert(nameParts);
+    }
+
+    private static String invert(List<String> nameParts) {
         String first = nameParts.get(0);
         String last = nameParts.get(1);
 
