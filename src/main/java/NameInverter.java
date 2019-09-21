465fd514577759e81ece263032a2a32eebafc229
joseph townshend <joetownshend@mac.com>
2019-09-21 10:45:33 +0100

    feat: can return a last name then first name seperated by comma

--- src/main/java/NameInverter.java
+++ src/main/java/NameInverter.java
@@ -7,8 +7,12 @@ public class NameInverter {
     public static String invert(String name) {
         List<String> nameParts = Lists.newArrayList(name.trim().split(" "));
 
+        if (nameParts.size() < 2)
+            return nameParts.get(0);
+
         String first = nameParts.get(0);
         String last = nameParts.get(1);
+        
         return String.format("%s, %s", last, first);
     }
 }
